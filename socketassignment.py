5510fd22606001a55c3403763dae9bddfe61ffc9
vignon <maxtwerpin@gmail.com>
2016-10-01 13:45:32 -0400

    12.1 socket assignemnt w/ errorcheck

--- socketassignment.py
+++ socketassignment.py
@@ -5,11 +5,14 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('www.data.pr4e.org', 80))
 s.send('GET http://data.pr4e.org/intro-short.txt HTTP/1.0\n\n')
 
-data = s.recv(512)
-str = data.split('\r\n')
-print 'Content Type:', str[1]
-print 'Content Length', str[2]
-print 'Last Modified', str[6]
-print 'Etag', str[7]
+while True:
+  data = s.recv(512)
+  if len(data) < 1 : break
+  str = data.split('\r\n')
+  print 'Content Type:', str[1]
+  print 'Content Length', str[2]
+  print 'Last Modified', str[6]
+  print 'Etag', str[7]
+  break
 
 s.close()
