358482309bc66adbbe2c1d95a0b499dabe340498
vignon <maxtwerpin@gmail.com>
2016-10-05 22:02:31 -0400

    loop change and string add

--- 2soupassignment.py
+++ 2soupassignment.py
@@ -4,17 +4,18 @@ import urllib
 url = raw_input('Enter URL: ')
 count = int(raw_input('Enter count: '))
 pos = int(raw_input('Enter position: '))
+num = 0
 names = []
 
-while count > 0:
+for num in range(count):
   print 'Retrieving: {0}'.format(url)
   page = urllib.urlopen(url)
   soup = BeautifulSoup(page, "html.parser")
   tags = soup('a')
-  data = tags[pos -1]
+  data = tags[pos -1].string
   names.append(data)
   url = tags[pos - 1]['href']
-  count = count - 1
+  num += 1
 
 print names[-1]
 
